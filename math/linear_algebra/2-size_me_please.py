31120151238c2c96edf20a3749457efbd5fb8008
martin Ndungu <gichurindungu@gmail.com>
2023-09-04 23:08:00 +0300

    alternative shape using numpy

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 def matrix_shape(matrix):
     shape = []
     if type(matrix[0]) == list and len(matrix[0]) == 2:
@@ -17,3 +18,11 @@ def matrix_shape(matrix):
         shape.append(num_rows)
         shape.append(num_of_columns)
     return shape
+def matrix_nam(matrix):
+    shape = list(np.shape(matrix))
+  
+    return shape
+
+matrix =  [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+print(matrix_nam(matrix))
