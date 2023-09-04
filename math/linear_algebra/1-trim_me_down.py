aa93e660bd32460b70378d761ee041679c68daef
martin Ndungu <gichurindungu@gmail.com>
2023-09-04 21:28:20 +0300

    start matrix trim

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+for element in matrix:
+    new_element = element[2:5]
+    print(new_element)
+
+
+#print("The middle columns of the matrix are: {}".format(the_middle))
