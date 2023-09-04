816cede8017c4525248caa66f0d60820abe121b4
martin Ndungu <gichurindungu@gmail.com>
2023-09-04 21:31:53 +0300

    final trim

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -2,8 +2,6 @@
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for element in matrix:
-    new_element = element[2:5]
-    print(new_element)
-
-
-#print("The middle columns of the matrix are: {}".format(the_middle))
+    new_element = element[2:4]
+    the_middle.append(new_element)
+print("The middle columns of the matrix are: {}".format(the_middle))
