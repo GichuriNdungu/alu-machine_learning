8a77c899b2bbc4a8a4acdbd78776039c9f0b7c3b
martin Ndungu <gichurindungu@gmail.com>
2023-09-04 22:59:32 +0300

    tentative shape calculator

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    shape = []
+    if type(matrix[0]) == list and len(matrix[0]) == 2:
+        shape.append(len(matrix))
+        shape.append(len(matrix[0]))
+    elif type(matrix[0]) == list:
+        num_rows = len(matrix)
+        num_of_columns = len(matrix[0])
+        spec_elements = len(matrix[0][0])
+        shape.append(num_rows)
+        shape.append(num_of_columns)
+        shape.append(spec_elements)
+    elif type(matrix) == list:
+        shape.append(len(matrix))
+    else:
+        shape.append(num_rows)
+        shape.append(num_of_columns)
+    return shape
+
+
+matrix = [[1, 2], [3, 4]]
+print(matrix_shape(matrix))
