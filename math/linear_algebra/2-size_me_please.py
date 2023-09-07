3ea072d6d638cce846255a56544dfae2f7aa77c9
martin Ndungu <gichurindungu@gmail.com>
2023-09-07 22:01:08 +0300

    debug 2d

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,24 +2,14 @@
 '''
 Matrix Shape Determination Function
 
-This script defines a function for determining the shape (dimensions) of a matrix. The function can determine the shape of a matrix whether it's a 1D, 2D, or 3D matrix.
+This script defines a function for determining the shape (dimensions) of a matrix.
+The function can determine the shape of a matrix whether it's a 1D, 2D, or 3D matrix.
 
 Usage:
     shape = matrix_shape(matrix)
 
 Parameters:
     matrix (list): The input matrix represented as a list of lists. It can be a 1D, 2D, or 3D matrix.
-
-Returns:
-    shape (list): A list representing the shape of the matrix:
-        - For a 1D matrix, the shape is [n], where n is the number of elements.
-        - For a 2D matrix, the shape is [m, n], where m is the number of rows and n is the number of columns.
-        - For a 3D matrix, the shape is [m, n, p], where m is the number of layers, n is the number of rows in each layer, and p is the number of columns in each layer.
-
-Notes:
-    - The function uses type checking and isinstance to determine the matrix's structure.
-    - It can handle matrices consisting of integers and floating-point numbers.
-
 Example:
     matrix = [[1, 2, 3], [4, 5, 6]]
     shape = matrix_shape(matrix)
