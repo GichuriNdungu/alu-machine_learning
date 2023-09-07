fabb7d0797a194cdf7f50acefd5605edbc64c24f
martin Ndungu <gichurindungu@gmail.com>
2023-09-07 21:59:30 +0300

    debug 2d

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -25,17 +25,19 @@ Example:
     shape = matrix_shape(matrix)
     print("Shape of the matrix:", shape)  # Output: [2, 3]
 '''
+
+
 def matrix_shape(matrix):
     '''determine the shape of a matrix'''
     shape = []
-    #check if matrix is 1d
+    # check if matrix is 1d
     if all(isinstance(obj, (int, float)) for obj in matrix):
         shape.append(len(matrix))
-    #check if matrix is 2d
+    # check if matrix is 2d
     elif type(matrix[0]) == list and type(matrix[0][0]) == int:
         shape.append(len(matrix))
         shape.append(len(matrix[0]))
-    #check if matrix is higher dimension
+    # check if matrix is higher dimension
     else:
         if not isinstance(matrix[0][0], list):
             return []
@@ -43,6 +45,6 @@ def matrix_shape(matrix):
             current_dimension_size = len(matrix)
             '''use recursion to determine the shape of the rem dimensions'''
             rem_dimensions_shape = matrix_shape(matrix[0])
-    
+
             shape = [current_dimension_size,] + rem_dimensions_shape
     return shape
