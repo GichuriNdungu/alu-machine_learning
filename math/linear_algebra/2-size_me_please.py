3a58044a25b4acbb35033ef2a96ac25ee8736e40
martin Ndungu <gichurindungu@gmail.com>
2023-09-07 20:04:56 +0300

    debug 2d

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-#import numpy as np
+
 def matrix_shape(matrix):
     shape = []
-    if type(matrix[0]) == list and len(matrix[0]) == 2:
+    if type(matrix[0]) == list and type(matrix[0][0]) == int:
         shape.append(len(matrix))
         shape.append(len(matrix[0]))
-    elif type(matrix[0]) == list:
+    elif type(matrix[0][0]) == list:
         num_rows = len(matrix)
         num_of_columns = len(matrix[0])
         spec_elements = len(matrix[0][0])
@@ -17,8 +17,4 @@ def matrix_shape(matrix):
     else:
         shape.append(num_rows)
         shape.append(num_of_columns)
-    return shape
-#def matrix_shape (matrix):
-    shape = list(np.shape(matrix))
-  
     return shape
