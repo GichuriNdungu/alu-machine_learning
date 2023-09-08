43e3d3f29cc36d40d3bcd0466ca95fcfa50f70ad
martin Ndungu <gichurindungu@gmail.com>
2023-09-08 21:21:58 +0300

    add 2d matrices

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -16,11 +16,3 @@ def add_matrices2D(mat1, mat2):
             row.append(mat1[i][j] + mat2[i][j])
         result.append(row)
     return result
-
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
