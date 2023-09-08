3633a7b3d0b95d23bd38592084f4dc04ec3ab200
martin Ndungu <gichurindungu@gmail.com>
2023-09-08 20:31:04 +0300

    add matrices debug

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -5,23 +5,8 @@ adds two arrays element-wise:'''
 
 
 def add_arrays(arr1, arr2):
-    # Check if the arrays have the same shape
     if len(arr1) != len(arr2):
         return None
-
-    # Initialize a result list to store the element-wise sum
-    result = []
-
-    # Perform element-wise addition and store the result
-    for i in range(len(arr1)):
-        result.append(arr1[i] + arr2[i])
-
-    return result
-
-
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
+    else:
+        new_list = list(map(sum, zip(arr1, arr2)))
+        return new_list
