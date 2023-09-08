b338292ce483f47517c59458af93dd16614b8057
martin Ndungu <gichurindungu@gmail.com>
2023-09-08 11:07:53 +0300

    transpose matrix

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-def transpose_matrix(matrix):
+def matrix_transpose(matrix):
     """
     Transpose a 2D matrix.
 
@@ -18,17 +18,5 @@ def transpose_matrix(matrix):
         # Output: [[1, 4], [2, 5], [3, 6]]
     """
     # Use the zip function to transpose the matrix
-    transposed = list(map(list, zip(*matrix)))
+    transposed =list(map(list, zip(*matrix)))
     return transposed
-
-
-matrix = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-          [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print(transpose_matrix(matrix))
-
-nested_list = [[1, 2], [3, 4], [5, 6]]
-
-# Unpack elements from the nested list
-unpacked_elements = [*nested_list[0], *nested_list[1], *nested_list[2]]
-
-print(unpacked_elements)
