449ff5ec242308c4fc7ac54a2db9245ee8737636
martin Ndungu <gichurindungu@gmail.com>
2023-09-08 21:28:51 +0300

    concatenate arrays)

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''function that concatenates two arrays'''
+
+
+def cat_arrays(arr1, arr2):
+    '''concatenate two arrays using + operator'''
+    result = arr1 + arr2
+    return result
