a6f12a00c85d9820fa48af9aa20367e7965c6223
martin Ndungu <gichurindungu@gmail.com>
2023-09-09 17:27:03 +0300

    concatenate 2d debug

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,8 +5,6 @@ along the same axis'''
 
 def cat_matrices2D(mat1, mat2, axis=0):
     '''contanate matrices on a given axis'''
-    if len(mat1) == 0 or len(mat2) == 0:
-        return None
     if axis == 0:
         result = []
         for row in mat1:
@@ -22,17 +20,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return result
     else:
         return None
-
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
-mat1[0] = [9, 10]
-mat1[1].append(5)
-print(mat1)
-print(mat4)
-print(mat5)
