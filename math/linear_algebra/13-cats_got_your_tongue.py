bc4c1ce766e5cbf13c2067fa378acf35792abd93
martin Ndungu <gichurindungu@gmail.com>
2023-09-10 12:01:22 +0300

    concatenate arrays

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -8,12 +8,4 @@ def np_cat(mat1, mat2, axis=0):
     '''args:
     mat 1, mat2, axis
     return: concatenated array'''
-    result = np.concatenate((mat1, mat2), axis)
-    return result
-
-mat1 = np.array([[11, 22, 33], [44, 55, 66]])
-mat2 = np.array([[1, 2, 3], [4, 5, 6]])
-mat3 = np.array([[7], [8]])
-print(np_cat(mat1, mat2))
-print(np_cat(mat1, mat2, axis=1))
-print(np_cat(mat1, mat3, axis=1))
+    return np.concatenate((mat1, mat2), axis)
