45691b6edfe5c8d63c9d523ddd76c1a007f0518d
martin Ndungu <gichurindungu@gmail.com>
2023-09-10 12:30:06 +0300

    dot matrix

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''function to perform matrix multiplication'''
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''args: mat1 , mat2
+    return: multupled func'''
+    return np.dot(mat1, mat2)
