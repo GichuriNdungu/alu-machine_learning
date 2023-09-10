af2abb75ea3f7ca91dbc48e101dd845b92691f29
gichuri <gichurindungu@gmail.com>
2023-09-10 02:16:16 -0700

    correct

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-import numpy as np 
+import numpy as np
 '''This function concatenates two matrices on a given axis
 params: matrix 1, matrix 2, axis
 returns the concatenated array
 '''
-def np_cat(mat1, mat2, axis-0):
+
+
+def np_cat(mat1, mat2, axis=0):
 	'''concatenates 2 arrays on agiven axis
 	returns a concatenated array
 	type(array) = np.ndarray'''
 	return np.concatenate((mat1, mat2), axis))
-
