930409cdd6b4ddca044422a079a2c6665fc8e03e
martin Ndungu <gichurindungu@gmail.com>
2023-09-10 12:03:51 +0300

    concatenate arrays

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 import numpy as np
-'''function to concatenate 2 matrices
-on a specific axis'''
+"""
+    Concatenates two matrices along a specific axis.
+
+    Parameters:
+        mat1 (numpy.ndarray):
+        mat2 (numpy.ndarray):
+        axis (int, optional):
+
+    Returns:
+        numpy.ndarray: The concatenated array.
+
+    Example:
+        mat1 = np.array([[1, 2], [3, 4]])
+        mat2 = np.array([[5, 6]])
+        result = np_cat(mat1, mat2, axis=0)
+        print(result)
+    """
 
 
 def np_cat(mat1, mat2, axis=0):
