3a6017e35660798747769099917558f38fdae38c
martin Ndungu <gichurindungu@gmail.com>
2023-09-16 16:01:36 +0300

    scatter plot trial

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+
+x = np.random.randn(2000) * 10
+y = np.random.randn(2000) * 10
+z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+plt.xlabel('x coordinate (m)')
+plt.ylabel('y-coordinate(m)')
+plt.title('Mountain Elevation')
+scatter = plt.scatter(x, y, c=z,cmap='viridis', label='elevation(m)')
+cbar = plt.colorbar(scatter)
+plt.show()
