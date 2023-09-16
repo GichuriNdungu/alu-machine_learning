eb6b1aa791ce3f8b7ac451f36e94e44e72fdc47a
martin Ndungu <gichurindungu@gmail.com>
2023-09-16 16:05:26 +0300

    color bar challenge

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -8,8 +8,9 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 plt.xlabel('x coordinate (m)')
-plt.ylabel('y-coordinate(m)')
+plt.ylabel('y coordinate(m)')
 plt.title('Mountain Elevation')
-scatter = plt.scatter(x, y, c=z,cmap='viridis', label='elevation(m)')
+scatter = plt.scatter(x, y, c=z, cmap='viridis', label='elevation(m)')
 cbar = plt.colorbar(scatter)
+cbar.set_label('elevation(m)')
 plt.show()
