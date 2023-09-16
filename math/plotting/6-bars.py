3af38195687dd8dd6ff39e43606b01c5cdd4e5d4
martin Ndungu <gichurindungu@gmail.com>
2023-09-16 15:48:18 +0300

    correct pep8

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,34 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
-#create a list of labels for the categories
+# create a list of labels for the categories
 
 categories = ['Farrah', 'Fred', 'Felicia']
-#generate a list of colors
+# generate a list of colors
 colors = ['red', 'Yellow', '#ff8000', '#ffe5b4']
-#create a list of quantities
+# create a list of quantities
 quantities = ['apples', 'bananas', 'oranges', 'peaches']
-#create an array to hold the bottom values for each bar
-#np.zeros creates a list of 0's
+# create an array to hold the bottom values for each bar
+# np.zeros creates a list of 0's
 
 bottom = np.zeros(len(categories))
 
-#create a figure and an axis
+# create a figure and an axis
 
 fig, ax = plt.subplots()
 
-#Loop through each row in the array (each row represents the quantities of the fruits)
+# Loop through each row in the array (each row represents the quantities of the fruits)
 for fruit_no in range(len(fruit)):
-    ax.bar(categories, fruit[fruit_no], width=0.5, label=quantities[fruit_no], bottom=bottom, color=colors[fruit_no])
+    ax.bar(categories, fruit[fruit_no], width=0.5,
+           label=quantities[fruit_no], bottom=bottom, color=colors[fruit_no])
     bottom += fruit[fruit_no]
-#add a label and a legend
+# add a label and a legend
 ax.set_xlabel('categories')
 ax.set_ylabel('Quantity')
 ax.set_title('Number of Fruit per Person')
 ax.set_ylim(0, 80)
 ax.legend()
 
-#show plot
+# show plot
 plt.show()
