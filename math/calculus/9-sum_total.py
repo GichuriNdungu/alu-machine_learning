6f431b71f8d6dd8fc851c935869128779703161a
martin Ndungu <gichurindungu@gmail.com>
2023-09-19 15:26:35 +0300

    documentatiion correct

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''function to get the summation of a squares
-\sum_{i=1}^{n} i^2:'''
+'''
 
 
 def summation_i_squared(n):
     '''function to get the summation of a squares
-\sum_{i=1}^{n} i^2:'''
+'''
     if type(n) != int:
         return None
     else:
