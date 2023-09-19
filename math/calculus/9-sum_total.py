75c216c42ec861d651373ea0592e03d6c61d4fc2
martin Ndungu <gichurindungu@gmail.com>
2023-09-19 15:22:22 +0300

    debug

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def summation_i_squared(n):
-     if type(n) != int:
-          return None
-     else:
+    if type(n) != int:
+        return None
+    else:
         squared_num = n*((n+1)*(2*n+1))//6
         return squared_num
