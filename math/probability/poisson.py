93b05504943a24f6f7f98e5feef2bb9f343dfd25
martin Ndungu <gichurindungu@gmail.com>
2023-09-28 11:49:34 +0300

    add shebang

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import math as mt
 class Poisson:
     def __init__(self, data=None, lambtha=1.):
-        sum_of_data = sum(data)
-        self.lambtha = float(sum_of_data/len(data))
-
-data = [1,2,3,4,5]
-print(len(data))
-p1 = Poisson(data=data)
-print(p1.lambtha)
+        if data == None:
+            self.lambtha = lambtha
+        else:
+            sum_of_data = sum(data)
+            self.lambtha = float(sum_of_data/len(data))
