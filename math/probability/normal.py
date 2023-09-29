5677edc661cb72ad857972e4af58d3e14863cc1c
martin Ndungu <gichurindungu@gmail.com>
2023-09-29 23:14:12 +0300

    normal distribution

---
+++ math/probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+'''class Normal that represents a normal distribution'''
+class Normal:
+    def __init__(self, data=None, mean=0., stddev=1.):
+        '''class constructor'''
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddeve must be a positive value')
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if type(data) != list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            else:
+                mean = sum(data)/len(data)
+                self.mean = mean
+                squared_diff_sum = 0
+                for i in data:
+                    squared_diff = (i-self.mean)**2
+                    squared_diff_sum += squared_diff
+                starndard_dev = (squared_diff_sum/len(data)) ** 0.5
+                self.stddev = starndard_dev
+                 
