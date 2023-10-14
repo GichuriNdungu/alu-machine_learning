03f47f323f10235a680119a0885722ace29889af
martin Ndungu <gichurindungu@gmail.com>
2023-10-14 19:20:04 +0300

    debug

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,31 +1,55 @@
 #!/usr/bin/env python3
+'''A class that calculates the determinant of a matrix'''
+
+
 def determinant(matrix):
-    '''check whether matrix is a list of lists'''
-    if len(matrix) == 0:
+    '''calculates the determinant of a matris
+    args: matrix
+    return: determinant'''
+    if not isinstance(matrix, list):
         raise TypeError('matrix must be a list of lists')
-    if len(matrix[0]) == 0:
-        return 1
-    elif len(matrix[0]) == 1:
-        return matrix[0][0]
-    elif isinstance(matrix, list):
-        '''check whether matrix is a square'''
-        for row in matrix:
-            if not isinstance(row, list):
-                raise TypeError('matrix must be a list of lists')
-        if len(matrix[0]) != len(matrix):
+    n = len(matrix)
+    if n == 0:
+        raise TypeError('matrix must be a list of lists')
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError('matrix must be a list of lists')
+        if len(row) == 0 and n == 1:
+            return 1
+        if len(row) != n:
             raise ValueError('matrix must be a square matrix')
-        else:
-            if len(matrix) == 2:
-                a = matrix[0][0] * matrix[1][1]
-                b = matrix[0][1] * matrix[1][0]
-                determinant = a - b
-                return determinant
-            else:
-                a = matrix[0][0] * ((matrix[1][1] * (matrix[2][2])) -
-                                    ((matrix[1][2]) * (matrix[2][1])))
-                b = matrix[0][1] * ((matrix[1][0] * (matrix[2][2])) -
-                                    ((matrix[1][2]) * (matrix[2][0])))
-                c = matrix[0][2] * ((matrix[1][0] * (matrix[2][1])) -
-                                    ((matrix[1][1]) * (matrix[2][0])))
-                determinant = a - b + c
-                return determinant
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        a = matrix[0][0]
+        b = matrix[0][1]
+        c = matrix[1][0]
+        d = matrix[1][1]
+        return (a*d) - (b*c)
+    elif n == 3:
+        a = matrix[0][0] * ((matrix[1][1] * (matrix[2][2])) -
+                            ((matrix[1][2]) * (matrix[2][1])))
+        b = matrix[0][1] * ((matrix[1][0] * (matrix[2][2])) -
+                            ((matrix[1][2]) * (matrix[2][0])))
+        c = matrix[0][2] * ((matrix[1][0] * (matrix[2][1])) -
+                            ((matrix[1][1]) * (matrix[2][0])))
+        determinant_value = a - b + c
+        return determinant_value
+    else:
+        multiplier = 1
+        d = 0
+        for j in range(n):
+            element = matrix[0][j]
+            submatrix = []
+            for row in range(n):
+                if row == 0:
+                    continue
+                new_row = []
+                for column in range(n):
+                    if column == j:
+                        continue
+                    new_row.append(matrix[row][column])
+                submatrix.append(new_row)
+            d += (element * determinant(submatrix))
+            multiplier *= -1
+        return d
--- math/advanced_linear_algebra/tests2.py
+++ math/advanced_linear_algebra/tests2.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
-matrix = [[1,2], 
-          [3,4]]
-a = matrix[0][0]* matrix[1][1]
-b = matrix[0][1]* matrix[1][0]
-determinant =a -b
-print(determinant)
+mat4 = [[5, 7, 9, 3, 4], 
+          [3, 1, 8, 4, 5], 
+          [6, 2, 4, 5, 8],
+          [3, 1, 8, 4, 5],
+          [5, 7, 9, 3, 4] ]
+n = len(matrix)
+multiplier = 1
+d = 0
+for j in range(n):
+    print(f'this is j {j}')
+    element = matrix[0][j]
+    print(f'this is the element {element}')
+    submatrix = []
+    for row in range(n):
+        if row == 0:
+            continue
+        new_row = []
+        for column in range(n):
+            if column == j:
+                continue
+            new_row.append(matrix[row][column])
+        submatrix.append(new_row)
+        print(f'this is the submatrix {submatrix}')
+        # d += (element * determinant(submatrix))
+        # multiplier *= -1
+        # return (d)
