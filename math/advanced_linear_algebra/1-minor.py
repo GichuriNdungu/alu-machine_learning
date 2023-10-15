177c7fda3b50cf683650a4205538580ecbc4a246
martin Ndungu <gichurindungu@gmail.com>
2023-10-15 12:41:14 +0300

    debug

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -4,13 +4,11 @@
 
 def minor(matrix):
     '''extract the minor of matrix'''
-    if not isinstance(matrix, list):
+    if type(matrix) != list:
         raise TypeError('matrix must be a list of lists')
     n = len(matrix)
     if n == 0:
         raise TypeError('matrix must be a list of lists')
-    if n == 1:
-        return [[1]]
     for row in matrix:
         if type(row) != list:
             raise TypeError('matrix must be a list of lists')
@@ -18,6 +16,8 @@ def minor(matrix):
             raise ValueError('matrix must be a non-empty square matrix')
         if len(matrix[0]) == 0:
             raise ValueError('matrix must be a non-empty square matrix')
+    if n == 1:
+        return [[1]]
     # if n == 2:
     #     minor = []
     #     row_a = [matrix[1][1], matrix[1][0]]
