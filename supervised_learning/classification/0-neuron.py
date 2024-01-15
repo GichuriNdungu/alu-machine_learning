6c3970b84142de462346412a691ec71c50c141ab
martin <martin--force-badname@DESKTOP-CF4DELR.localdomain>
2024-01-15 19:50:35 +0200

    binary net

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,12 +1,19 @@
+#!/usr/bin/env python3
+'''Defines a binary classification class'''
 import numpy as np
 
+
 class Neuron:
+    '''class neuron representing a single neuron in an NN'''
+
     def __init__(self, nx):
         '''nx refers to the number of input features'''
         if type(nx) != int:
             raise Exception('nx must be an integer')
         if nx < 1:
             raise Exception('nx must be a positive integer')
-        self.w = np.random.normal()
+        '''declare a vector size'''
+        vector_size = np.random.randint(1, 100)
+        self.W = np.random.normal(size=vector_size)
         self.b = 0
         self.A = 0
