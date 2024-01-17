19ca218ff9847b214acb803b86e0cc330f44819b
Martin <gichurindungu@gmail.com>
2024-01-17 05:56:39 +0200

    private inst att

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -12,19 +12,20 @@ class Neuron:
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError('nx must be a positive integer')
-                
+            raise ValueError('nx must be a positive integer')        
         '''declare private instance attributes'''
-        self.__w = np.random.randn(1, nx)
+        self.__W = np.random.randn(1, nx)
         self.__b = 0 
         self.__A = 0
-
+    @property
     def get_w(self):
         '''getter method for self.__w'''
         return self.__w
+    @property
     def get_b(self):
         '''getter method for private instace b'''
         return self.__b
+    @property
     def get_A(self):
         '''getter method for private instance A'''
         return self.__A
--- supervised_learning/classification/test.py
+++ supervised_learning/classification/test.py
@@ -2,16 +2,15 @@
 
 import numpy as np
 
+Neuron = __import__('1-neuron').Neuron
 
-Neuron = __import__('0-neuron').Neuron
-
-lib_train = np.load('.\data\Binary_Dev.npz')
+lib_train = np.load('./data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
 X = X_3D.reshape((X_3D.shape[0], -1)).T
+
 np.random.seed(0)
 neuron = Neuron(X.shape[0])
 print(neuron.W)
-print(neuron.W.shape)
 print(neuron.b)
 print(neuron.A)
 neuron.A = 10
