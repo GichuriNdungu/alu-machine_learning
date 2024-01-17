757d8f7f36bfab0c56a6c1a5fc77d2ce51b48b87
Martin <gichurindungu@gmail.com>
2024-01-17 06:02:56 +0200

    debug getter function

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -6,26 +6,28 @@ import numpy as np
 class Neuron:
     '''class neuron representing a single neuron in an NN'''
 
-
     def __init__(self, nx):
         '''nx refers to the number of input features'''
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError('nx must be a positive integer')        
+            raise ValueError('nx must be a positive integer')
         '''declare private instance attributes'''
         self.__W = np.random.randn(1, nx)
-        self.__b = 0 
+        self.__b = 0
         self.__A = 0
+
     @property
-    def get_w(self):
+    def W(self):
         '''getter method for self.__w'''
-        return self.__w
+        return self.__W
+
     @property
-    def get_b(self):
+    def b(self):
         '''getter method for private instace b'''
         return self.__b
+
     @property
-    def get_A(self):
+    def A(self):
         '''getter method for private instance A'''
         return self.__A
