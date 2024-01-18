5757954d133ee2bd734500782e138e01703c3654
Martin <gichurindungu@gmail.com>
2024-01-18 22:17:17 +0200

    evaluate neuron

--- supervised_learning/classification/test.py
+++ supervised_learning/classification/test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+
 import numpy as np
 
-NN = __import__('11-neural_network').NeuralNetwork
+NN = __import__('12-neural_network').NeuralNetwork
 
 lib_train = np.load('./data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
@@ -9,6 +10,6 @@ X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
 nn = NN(X.shape[0], 3)
-_, A = nn.forward_prop(X)
-cost = nn.cost(Y, A)
+A, cost = nn.evaluate(X, Y)
+print(A)
 print(cost)
