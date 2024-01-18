80282b5cdb4a89c895468185c75f27d136a5304d
Martin <gichurindungu@gmail.com>
2024-01-18 21:39:27 +0200

    forward propagation

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+'''Defines a Neural Network with one hidden
+layer performing binary classification'''
+
+import numpy as np
+
+
+class NeuralNetwork:
+    '''class Neural Network defining a neural net with one hidden layer'''
+
+    def __init__(self, nx, nodes):
+        '''class constructor'''
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        '''getter function for W1'''
+        return self.__W1
+
+    @property
+    def b1(self):
+        '''getter function for b1'''
+        return self.__b1
+
+    @property
+    def A1(self):
+        '''getter function for A1'''
+        return self.__A1
+
+    @property
+    def W2(self):
+        '''getter function for W2'''
+        return self.__W2
+
+    @property
+    def b2(self):
+        '''getter function for b2'''
+        return self.__b2
+
+    @property
+    def A2(self):
+        '''getter function for A2'''
+        return self.__A2
+    
+    def forward_prop(self, X):
+        '''calculates the forward propagation of the neural network'''
+
+        '''multiply transposed weights with input vector'''
+        weighted_sum1 = np.dot(self.W1, X)
+        ''' Add the bias to the dot'''
+        Z1 = np.add(weighted_sum1, self.b1)
+        '''apply the activation function, sigmoid
+        to get the output for the hidden layer'''
+
+        self.__A1 = 1/(1+np.exp(-Z1))
+
+        ''''output from the hidden layer will be 
+        the input for the output neuron'''
+
+        weighted_sum_2 = np.dot(self.W2, self.__A1)
+        Z2 = np.add(weighted_sum_2, self.b2)
+        self.__A2 = 1/(1+np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+
--- supervised_learning/classification/test.py
+++ supervised_learning/classification/test.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-NN = __import__('9-neural_network').NeuralNetwork
+NN = __import__('10-neural_network').NeuralNetwork
 
 lib_train = np.load('./data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
@@ -10,10 +10,10 @@ X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
 nn = NN(X.shape[0], 3)
-print(nn.W1)
-print(nn.b1)
-print(nn.W2)
-print(nn.b2)
-print(nn.A1)
-print(nn.A2)
-nn.A1 = 10
+nn._NeuralNetwork__b1 = np.ones((3, 1))
+nn._NeuralNetwork__b2 = 1
+A1, A2 = nn.forward_prop(X)
+if A1 is nn.A1:
+        print(A1)
+if A2 is nn.A2:
+        print(A2)
