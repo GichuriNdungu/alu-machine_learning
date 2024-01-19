aff2eb54b6e31af899adfa7dadbf37466109133a
Martin <gichurindungu@gmail.com>
2024-01-19 10:26:43 +0200

    correct min

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -15,9 +15,9 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
-        self.L = len(layers)
-        if self.L == 0:
+        if min(layers) < 0:
             raise TypeError('layers must be a list of positive integers')
+        self.L = len(layers)
         self.cache = {}
         self.weights = {}
         for layer in range(1, self.L):
