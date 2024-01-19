6850087d00f9fda0b011f4b8cb6d39c9e2d06dca
Martin <gichurindungu@gmail.com>
2024-01-19 22:57:13 +0200

    back propagate

--- supervised_learning/classification/test.py
+++ supervised_learning/classification/test.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-Deep = __import__('20-deep_neural_network').DeepNeuralNetwork
+Deep = __import__('21-deep_neural_network').DeepNeuralNetwork
 
 lib_train = np.load('./data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
@@ -10,6 +10,6 @@ X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
 deep = Deep(X.shape[0], [5, 3, 1])
-A, cost = deep.evaluate(X, Y)
-print(A)
-print(cost)
+A, cache = deep.forward_prop(X)
+deep.gradient_descent(Y, cache, 0.5)
+print(deep.weights)
