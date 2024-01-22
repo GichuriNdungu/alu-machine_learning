622e0c7800cbe29d7c77d58525069013a0e20b2d
Martin <gichurindungu@gmail.com>
2024-01-22 16:15:50 +0200

    correct pep8

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 ''''function that returns two placeholders for our DNN'''
+
+
 def create_placeholders(nx, classes):
     '''function that returns two placeholders,
       x and y, for the neural network
-      
+
       args: nx: the number of feature columns in our data
             classes: the number of classes in our classifier
             returns: placeholders named x and y, respectively'''
