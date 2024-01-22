901c7df567b33fe21aa628e189ab9b3b2efeada4
Martin <gichurindungu@gmail.com>
2024-01-22 19:03:39 +0200

    correct pep8

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def create_train_op(loss, alpha):
     '''args:loss:loss of the network’s prediction
             alpha:learning rate
-            returns:an operation that trains 
+            returns:an operation that trains
             the network using gradient descent'''
     optimizer = tf.train.GradientDescentOptimizer(alpha)
     train_op = optimizer.minimize(loss)
