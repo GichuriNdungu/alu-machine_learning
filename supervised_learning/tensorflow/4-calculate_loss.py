f3212a941700a4f4047824024f4d00ca31a71c4d
Martin <gichurindungu@gmail.com>
2024-01-22 18:49:35 +0200

    loss function

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -7,6 +7,6 @@ def calculate_loss(y, y_pred):
     '''args:y:placeholder for the labels of the input data
             y_pred:tensor containing the network’s predictions
             returns:tensor containing the loss of the prediction'''
-    loss = tf.mean(tf.nn.softmax_cross_entropy_with_logits(
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
         labels=y, logits=y_pred))
     return loss
