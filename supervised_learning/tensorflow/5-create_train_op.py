332144af1a68cf75026aa1693e0941e04f9fb4e8
Martin <gichurindungu@gmail.com>
2024-01-22 19:02:37 +0200

    train model

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 def create_train_op(loss, alpha):
     '''args:loss:loss of the network’s prediction
             alpha:learning rate
-            returns:an operation that trains the network using gradient descent'''
+            returns:an operation that trains 
+            the network using gradient descent'''
     optimizer = tf.train.GradientDescentOptimizer(alpha)
     train_op = optimizer.minimize(loss)
     return train_op
