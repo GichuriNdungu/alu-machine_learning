48058079d21c9157264cca5391fcf3528e6fff00
Martin <gichurindungu@gmail.com>
2024-01-26 18:20:46 +0200

    train

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -10,6 +10,6 @@ def create_placeholders(nx, classes):
       args: nx: the number of feature columns in our data
             classes: the number of classes in our classifier
             returns: placeholders named x and y, respectively'''
-    x = tf.placeholder('float', [None, nx], name='x')
-    y = tf.placeholder('float', classes, name='y')
+    x = tf.compat.v1.placeholder('float', [None, nx], name='x')
+    y = tf.compat.v1.placeholder('float', classes, name='y')
     return x, y
