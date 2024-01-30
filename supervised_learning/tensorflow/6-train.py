e67af20286811da9a0b853e35125f436023eca26
Martin <gichurindungu@gmail.com>
2024-01-30 13:07:50 +0200

    train loop

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python3'''
-'''function that builds, trains, and saves a neural network classifier'''
+#!/usr/bin/env python3
+""" train"""
 
+
+import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
-import tensorflow as tf
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     '''args: X_train: training input data
     Y_train: training labels
     X_valid: validation input data
@@ -19,37 +22,36 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     iterations: number of iterations to train over
     save_path: designates where to save the model
     returns: the path where the model was saved'''
-    # Create placeholders
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    # Forward propagation
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
     y_pred = forward_prop(x, layer_sizes, activations)
-    # Calculate accuracy
+    tf.add_to_collection('y_pred', y_pred)
     accuracy = calculate_accuracy(y, y_pred)
-    # Calculate loss
+    tf.add_to_collection('accuracy', accuracy)
     loss = calculate_loss(y, y_pred)
-    # Create train operation
+    tf.add_to_collection('loss', loss)
     train_op = create_train_op(loss, alpha)
-    # Initialize variables
+    tf.add_to_collection('train_op', train_op)
     init = tf.global_variables_initializer()
-    # Create saver
     saver = tf.train.Saver()
-    # Create session
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(iterations):
-            # Calculate cost and accuracy for training data
-            cost_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            accuracy_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-            # Calculate cost and accuracy for validation data
-            cost_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            accuracy_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
-
-            # Print costs and accuracies
-            if i % 100 == 0 or i == 0:
+        for i in range(iterations + 1):
+            cost_t = sess.run(loss,
+                              feed_dict={x: X_train, y: Y_train})
+            acc_t = sess.run(accuracy,
+                             feed_dict={x: X_train, y: Y_train})
+            cost_v = sess.run(loss,
+                              feed_dict={x: X_valid, y: Y_valid})
+            acc_v = sess.run(accuracy,
+                             feed_dict={x: X_valid, y: Y_valid})
+            if i % 100 == 0 or i == iterations:
                 print("After {} iterations:".format(i))
-                print("\tTraining Cost: {}".format(cost_train))
-                print("\tTraining Accuracy: {}".format(accuracy_train))
-                print("\t Validation Cost: {}".format(cost_valid))
-                print("\t Validation Accuracy: {}".format(accuracy_valid))
+                print("\tTraining Cost: {}".format(cost_t))
+                print("\tTraining Accuracy: {}".format(acc_t))
+                print("\tValidation Cost: {}".format(cost_v))
+                print("\tValidation Accuracy: {}".format(acc_v))
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         return saver.save(sess, save_path)
