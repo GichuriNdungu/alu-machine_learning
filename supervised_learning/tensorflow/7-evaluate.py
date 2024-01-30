d100a173cb96d67fd7805df51f131f4df4d94acb
Martin <gichurindungu@gmail.com>
2024-01-30 15:45:28 +0200

    load + evaluate

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -2,7 +2,8 @@
 '''function that evaluates the output of a network'''
 import tensorflow as tf
 
-def evaluate(X,Y,save_path):
+
+def evaluate(X, Y, save_path):
     '''args: x: input data
             Y: one-hot labels for x
         returns: networks pred, accuracy, loss'''
@@ -12,4 +13,9 @@ def evaluate(X,Y,save_path):
         '''restore saved variables'''
         saver.restore(sess, tf.train.latest_checkpoint(save_path))
         # access the tensors from the collection
-        
+
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        pred, acc, cost = sess.run(
+            [y_pred, accuracy, loss], feed_dict={X: X, Y: Y})
