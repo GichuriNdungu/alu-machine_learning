863eb37558da6f403e40a4db4b68bcb8b5f85e2f
Martin <gichurindungu@gmail.com>
2024-01-31 17:34:23 +0200

    correct bias

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -22,21 +22,23 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         # prev_out represents the output from the previous layer,
         # which is the input of our current layer
         prev_out = cache['A' + str(layer - 1)]
-        
+
         if layer == L:
-        #    get the error from the last layer
+            #    get the error from the last layer
             back['dz{}'.format(layer)] = (cache['A{}'.format(layer)]-Y)
         else:
             # get the error from the preceeding layer from the right
             dz_prev = back['dz{}'.format(layer+1)]
             # get the output from the current layer
-            A_current = cache['A'+ str(layer)]
+            A_current = cache['A' + str(layer)]
             # calculate and update the error for the current layer
-            back['dz{}'.format(layer)] = (np.matmul(curr_w.transpose(), dz_prev)* (A_current) * (1-A_current))
+            back['dz{}'.format(layer)] = (
+                np.matmul(curr_w.transpose(), dz_prev) * (A_current) * (1-A_current))
         # get the error in the current layer
         dz = back['dz{}'.format(layer)]
-        dw = (1/m) * ((np.matmul(dz, prev_out.T))) + (lambtha * weights['W{}'.format(layer)])
-        db = (1/m) * (np.sum(dz, axis=1, keepdims=True)) + (lambtha * weights['W{}'.format(layer)])
+        dw = (1/m) * ((np.matmul(dz, prev_out.T))) + \
+            (lambtha * weights['W{}'.format(layer)])
+        db = (1/m) * (np.sum(dz, axis=1, keepdims=True))
         curr_w = weights['W{}'.format(layer)]
         # update the weights and the biases
         weights['W' + str(layer)] -= alpha * dw
