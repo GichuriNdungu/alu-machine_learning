1656c392963824a0901ea2ef2056f8c689c1dc95
Martin <gichurindungu@gmail.com>
2024-01-31 17:36:46 +0200

    correct bias

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3
-''''function to update the
- weights and biases of a neural net using gradient descent'''
+"""
+Defines a function that updates the weights and biases
+using gradient descent with L2 Regularization
+"""
+
+
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    '''args: weights: dictionary of the weights and biases of the neural net
-       cache: dictionary of the outputs of each layer of the neural net
-       alpha: learning rate
-       lambtha: regularization parameter
-       L: number of layers of the network
-       Y: one-hot numpy.ndarray of shape (classes, m) that contains the
-          correct labels for the data
-       returns: nothing. updates the weights and biases of the network'''
-    # get the number of data points
-    m = Y.shape[1]
-    # define the output from each layer
-    back = {}
-    # kictart backward propagation
-    for layer in range(L, 0, -1):
-        # prev_out represents the output from the previous layer,
-        # which is the input of our current layer
-        prev_out = cache['A' + str(layer - 1)]
+    """
+    Updates weights and biases using gradient descent with L2 regularization
 
-        if layer == L:
-            #    get the error from the last layer
-            back['dz{}'.format(layer)] = (cache['A{}'.format(layer)]-Y)
-        else:
-            # get the error from the preceeding layer from the right
-            dz_prev = back['dz{}'.format(layer+1)]
-            # get the output from the current layer
-            A_current = cache['A' + str(layer)]
-            # calculate and update the error for the current layer
-            back['dz{}'.format(layer)] = (
-                np.matmul(curr_w.transpose(), dz_prev) * (A_current) * (1-A_current))
-        # get the error in the current layer
-        dz = back['dz{}'.format(layer)]
-        dw = (1/m) * ((np.matmul(dz, prev_out.T))) + \
-            (lambtha * weights['W{}'.format(layer)])
-        db = (1/m) * (np.sum(dz, axis=1, keepdims=True))
-        curr_w = weights['W{}'.format(layer)]
-        # update the weights and the biases
-        weights['W' + str(layer)] -= alpha * dw
-        weights['b' + str(layer)] -= alpha * db
+    parameters:
+        Y [one-hot numpy.ndarray of shape (classes, m)]:
+            contains the correct labels for the data
+            classes: number of classes
+            m: number of data points
+        weights [dict]: dictionary of weights and biases for the network
+        cache [dict]: dictionary of the outputs of each layer of the network
+        alpha [float]: learning rate
+        lambtha: the regularization parameter
+        L: the number of layers in the neural network
+
+    Neural network using tanh activations on each layer except the last.
+    Last layer uses softmax activation.
+    """
+
+    Args:
+        Y(numpy.ndarray): one-hot matrix with the correct labels
+        weights(dict): The weights and biases of the network
+        cache(dict): The outputs of each layer of the network
+        alpha(float): The learning rate
+        lambtha(float): The L2 regularization parameter
+        L(int): The number of layers of the network
+    """
+    m = Y.shape[1]
+    dz = cache['A' + str(L)] - Y
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        dw = (1 / m) * np.matmul(dz, A.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
+        dz = np.matmul(W.T, dz) * (A * (1 - A))
+        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dw
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
+
+    return weights
