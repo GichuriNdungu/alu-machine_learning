fb2290195e625f9c7b43144e513472005a353643
Martin <gichurindungu@gmail.com>
2024-01-31 17:40:33 +0200

    correct bias

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -4,7 +4,6 @@ Defines a function that updates the weights and biases
 using gradient descent with L2 Regularization
 """
 
-
 import numpy as np
 
 
@@ -24,17 +23,29 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         L: the number of layers in the neural network
 
     Neural network using tanh activations on each layer except the last.
-    Last layer uses softmax activation
+    Last layer uses softmax activation.
     """
     m = Y.shape[1]
-    dz = cache['A' + str(L)] - Y
-    for i in range(L, 0, -1):
-        A = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
-        dw = (1 / m) * np.matmul(dz, A.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        dz = np.matmul(W.T, dz) * (A * (1 - A))
-        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dw
-        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
-
-    return weights
+    back = {}
+    for index in range(L, 0, -1):
+        A = cache["A{}".format(index - 1)]
+        if index == L:
+            back["dz{}".format(index)] = (cache["A{}".format(index)] - Y)
+        else:
+            dz_prev = back["dz{}".format(index + 1)]
+            A_current = cache["A{}".format(index)]
+            back["dz{}".format(index)] = (
+                np.matmul(W_prev.transpose(), dz_prev) *
+                (A_current * (1 - A_current)))
+        dz = back["dz{}".format(index)]
+        dW = (1 / m) * (
+            (np.matmul(dz, A.transpose())) + (
+                lambtha * weights["W{}".format(index)]))
+        db = (1 / m) * (
+            (np.sum(dz, axis=1, keepdims=True)) + (
+                lambtha * weights["b{}".format(index)]))
+        W_prev = weights["W{}".format(index)]
+        weights["W{}".format(index)] = (
+            weights["W{}".format(index)] - (alpha * dW))
+        weights["b{}".format(index)] = (
+            weights["b{}".format(index)] - (alpha * db))
