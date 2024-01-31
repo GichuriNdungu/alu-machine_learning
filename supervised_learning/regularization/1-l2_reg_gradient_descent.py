e6bd41db77349593b0e6f07bb37a8f88acad6026
Martin <gichurindungu@gmail.com>
2024-01-31 17:26:03 +0200

    correct bias

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -38,8 +38,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         dz = back['dz{}'.format(layer)]
         dw = (1/m) * ((np.matmul(dz, prev_out.T))) + \
             (lambtha * weights['W{}'.format(layer)])
-        db = (1/m) * (np.sum(dz, axis=1, keepdims=True)) + \
-            (lambtha * weights['W{}'.format(layer)])
+        db = (1/m) * (np.sum(dz, axis=1, keepdims=True))
         curr_w = weights['W{}'.format(layer)]
         # update the weights and the biases
         weights['W' + str(layer)] -= alpha * dw
