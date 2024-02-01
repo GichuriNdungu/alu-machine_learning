f19090ee87aa55ddc24b128e670f276ddde4337b
Martin <gichurindungu@gmail.com>
2024-02-01 18:54:27 +0200

    apply dropout

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,49 +1,37 @@
 #!/usr/bin/env python3
-"""
-Defines function that conducts forward propagation using Dropout
-"""
-
-
+'''function that conducts forward propagation using Dropout:'''
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """
-    Conducts forward propagation using Dropout
-
-    parameters:
-        X [numpy.ndarray of shape(nx, m)]:
-            contains the input data for the network
-            nx: number of input features
-            m: number of data points
-        weights [dict]:
-            contains weights and biases of the network
-        L [int]:
-            number of layers in the network
-        keep_prob [float]:
-            probability that a node will be kept
-
-    all layers except last should use tanh activation function
-    last layer should use softmax activation function
-
-    returns:
-        dictionary containing the outputs of each layer and
-            the dropout mask used on each layer
-    """
+    ''''args: X: numpy.ndarray of shape (nx, m) containing the input data for
+    the network, nx: number of input features to the network, m: number of
+    data points, weights: dictionary of the weights and biases of the neural
+    network, L: number of layers in the network, keep_prob: probability that
+    a node will be kept'''
     outputs = {}
-    outputs["A0"] = X
-    for index in range(L):
-        weight = weights["W{}".format(index + 1)]
-        bias = weights["b{}".format(index + 1)]
-        z = np.matmul(weight, outputs["A{}".format(index)]) + bias
+    outputs['A0'] = X
+    for layer in range(L):
+        # define the weights and biases
+        weight = weights['W{}'.format(layer+1)]
+        bias = weights['b{}'.format(layer+1)]
+        # calculate layer output as before
+        linear_reg = np.dot(weight, outputs['A{}'.format(layer)])
+        z = np.add(linear_reg, bias)
+        # Get randomized 1 and 0's
         dropout = np.random.binomial(1, keep_prob, size=z.shape)
-        if index != (L - 1):
+        #  check if layer is final
+        if layer < L:
+            # apply tanh activation
             A = np.tanh(z)
+            # multiply the output of the layer to the dropout
             A *= dropout
+            # scale A
             A /= keep_prob
-            outputs["D{}".format(index + 1)] = dropout
+            # outputs['D{}'.format(layer)] = dropout
         else:
+            # apply softmax activation for the output layer
             A = np.exp(z)
             A /= np.sum(A, axis=0, keepdims=True)
-        outputs["A{}".format(index + 1)] = A
+        outputs['A{}'.format(layer+1)] = A
     return outputs
