9e8a376fe57b8f3e487770b4eaddc3a5fb5614d6
Martin <gichurindungu@gmail.com>
2024-02-02 17:22:14 +0200

    correct function name

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
-"""Script to implement dropout in tensorflow"""
-
+'''Function that creates a layer with l2 regularization'''
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-    """
-    Function that uses dropout in tensorflow
-    Args:
-        prev: tensor containing the output of the previous layer
-        n: number of nodes the new layer should contain
-        activation: activation function that should be used on the layer
-        keep_prob: probability that a node will be kept
-
-    Returns: output of the new layer
-
-    """
-    dropout = tf.layers.Dropout(keep_prob)
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    tensor = tf.layers.Dense(units=n, activation=activation,
-                             kernel_initializer=init,
-                             kernel_regularizer=dropout)
-    return tensor(prev)
+    '''args: prev: output from previous layer,
+    n: nodes in layer,
+    activation: activation function for our layer,
+    keep_prob: probability that a node will be kept'''
+    initializer = tf.contrib.layers.variance_scaling_initializer(
+        mode="FAN_AVG")
+    dropout = tf.contrib.layers.Dropout(keep_prob)
+    hidden_layer = tf.layers.Dense(units=n, activation=activation,
+                                   kernel_initializer=initializer,
+                                   kernel_regularizer=dropout)
+    output = hidden_layer(prev)
+    return output
