34d6ceca4ce1a1eef4e3c526743b87178eca21fe
Martin <gichurindungu@gmail.com>
2024-02-04 19:49:52 +0200

    remove loop

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -5,8 +5,7 @@
 def normalize(X, m, s):
     '''args: X; input
     m: mean of X
-    s: standard dev of x 
+    s: standard dev of x
     returns: Z_score(standardized X)'''
-    for element in range(len(X)):
-        z = (X[element-1]-m)/s
-    return z
+    z_score = (X-m)/s
+    return z_score
