9d48ff2e91bff944d668c2d1686ad8a0dcbf5b9e
Martin <gichurindungu@gmail.com>
2024-02-05 15:04:45 +0200

    correct pep8

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     '''args: alpha: initial learning rate
             decay_rate: weight to determin the rate of decay
-            global_step: number of passes of gradient descent that have lapsed 
+            global_step: number of passes of gradient descent that have lapsed
             decay_step: number of passes of gradient descent before decay
             returns: updated alpha after each update'''
     return tf.train.inverse_time_decay(
