cd1d1b2bea6a51187d049d7f3fdc4ae427b9276d
Martin <gichurindungu@gmail.com>
2024-02-05 02:20:00 +0200

    root mean squared propagation

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''function that updates variables using rmsprop algorithm'''
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epilson, var, grad, s):
+    '''args: alpha: learning rate
+            beta2: rmsprop weight
+            epilson: value to prevent 0-division
+            var: variable to update
+            grad: gradient descent of var
+            s: previous second moment of var
+            return: updated variable, new moment'''
+
+    sdw = s * beta2 + (1-beta2)*(grad**2)
+    dw_multplier = grad/np.sqrt(sdw+epilson)
+    new_value = var - (alpha*dw_multplier)
+    s = sdw
+    return new_value, sdw
--- supervised_learning/optimization/tests.py
+++ supervised_learning/optimization/tests.py
@@ -2,47 +2,56 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow as tf
-create_momentum_op = __import__('6-momentum').create_momentum_op
+update_variables_RMSProp = __import__('7-RMSProp').update_variables_RMSProp
 
-def one_hot(Y, classes):
-    """convert an array to a one-hot matrix"""
-    one_hot = np.zeros((Y.shape[0], classes))
-    one_hot[np.arange(Y.shape[0]), Y] = 1
-    return one_hot
+def forward_prop(X, W, b):
+    Z = np.matmul(X, W) + b
+    A = 1 / (1 + np.exp(-Z))
+    return A
+
+def calculate_grads(Y, A, W, b):
+    m = Y.shape[0]
+    dZ = A - Y
+    dW = np.matmul(X.T, dZ) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    return dW, db
+
+def calculate_cost(Y, A):
+    m = Y.shape[0]
+    loss = - (Y * np.log(A) + (1 - Y) * np.log(1 - A))
+    cost = np.sum(loss) / m
+
+    return cost
 
 if __name__ == '__main__':
-    lib= np.load('../data/MNIST.npz')
-    X_3D = lib['X_train']
-    Y = lib['Y_train']
+    lib_train = np.load('../data/Binary_Train.npz')
+    X_3D, Y = lib_train['X'], lib_train['Y'].T
     X = X_3D.reshape((X_3D.shape[0], -1))
-    Y_oh = one_hot(Y, 10)
 
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph('./graph.ckpt.meta')
-        saver.restore(sess, './graph.ckpt')
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        loss = tf.get_collection('loss')[0]
-        train_op = create_momentum_op(loss, 0.01, 0.9)
-        init = tf.global_variables_initializer()
-        sess.run(init)
-        for i in range(1000):
-            if not (i % 100):
-                cost = sess.run(loss, feed_dict={x:X, y:Y_oh})
-                print('Cost after {} iterations: {}'.format(i, cost))
-            sess.run(train_op, feed_dict={x:X, y:Y_oh})
-        cost, Y_pred_oh = sess.run((loss, y_pred), feed_dict={x:X, y:Y_oh})
-        print('Cost after {} iterations: {}'.format(1000, cost))
+    nx = X.shape[1]
+    np.random.seed(0)
+    W = np.random.randn(nx, 1)
+    b = 0
+    dW_prev = np.zeros((nx, 1))
+    db_prev = 0
+    for i in range(1000):
+        A = forward_prop(X, W, b)
+        if not (i % 100):
+            cost = calculate_cost(Y, A)
+            print('Cost after {} iterations: {}'.format(i, cost))
+        dW, db = calculate_grads(Y, A, W, b)
+        W, dW_prev = update_variables_RMSProp(0.001, 0.9, 1e-8, W, dW, dW_prev)
+        b, db_prev = update_variables_RMSProp(0.001, 0.9, 1e-8, b, db, db_prev)
+    A = forward_prop(X, W, b)
+    cost = calculate_cost(Y, A)
+    print('Cost after {} iterations: {}'.format(1000, cost))
 
-    Y_pred = np.argmax(Y_pred_oh, axis=1)
-
+    Y_pred = np.where(A >= 0.5, 1, 0)
     fig = plt.figure(figsize=(10, 10))
     for i in range(100):
         fig.add_subplot(10, 10, i + 1)
         plt.imshow(X_3D[i])
-        plt.title(str(Y_pred[i]))
+        plt.title(str(Y_pred[i, 0]))
         plt.axis('off')
     plt.tight_layout()
     plt.show()
