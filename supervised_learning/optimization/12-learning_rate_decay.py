f1e9205800f177d03166ecb4928245ef12163bb1
Martin <gichurindungu@gmail.com>
2024-02-05 15:01:38 +0200

    tensorflow learning rate decay

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -2,12 +2,18 @@
 '''function that implements learning rate decay'''
 import numpy as np
 
-
+import tensorflow as tf
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     '''args: alpha: initial learning rate
             decay_rate: weight to determin the rate of decay
-            global_step: number of passes of gradient descent that have lapsed
+            global_step: number of passes of gradient descent that have lapsed 
             decay_step: number of passes of gradient descent before decay
             returns: updated alpha after each update'''
-    alpha /= 1+(decay_rate*np.floor(global_step/decay_step))
-    return alpha
+    return tf.train.inverse_time_decay(
+        learning_rate=alpha,
+        global_step=global_step,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True)
+
+
