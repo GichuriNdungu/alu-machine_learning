5a5adf1d5cfd8c148ec67ea31e8d6748886ce5ff
Martin <gichurindungu@gmail.com>
2024-02-05 02:34:46 +0200

    tensorflow rmsprop

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''function that creates a training operation for
+ neural net in tensorflow
+ using grad descent with rmsprop optimization'''
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    '''args:loss;loss of the net
+            alpha: learning rate
+            beta2: rmsprop weight
+            return:rmsprop optimization operation'''
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2)
+    train_op = optimizer.minimize(loss)
+    return train_op
