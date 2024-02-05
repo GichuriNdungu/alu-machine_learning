9fc8c3e7a71cf6d2f36804a63a14d81f97f2fee4
Martin <gichurindungu@gmail.com>
2024-02-05 03:08:30 +0200

    tensorflow adam optimizer

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''function that creates a training operation for
+ neural net in tensorflow
+ using grad descent with adam optimization'''
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    '''args:loss;loss of the net
+            alpha: learning rate
+            beta2: rmsprop weight
+            return:rmsprop optimization operation'''
+    optimizer = tf.train.AdamOptimizer(
+        alpha, beta1, beta2, epsilon=epsilon).minimize(loss)
+    return optimizer
