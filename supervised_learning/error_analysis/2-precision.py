ba301468edf988125bce09dac0fa07fb44ee03a3
Martin <gichurindungu@gmail.com>
2024-02-14 12:48:30 +0200

    calculate sensitivity

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''function that calculates the
+sensitivity for each class in a confusion matrix'''
+import numpy as np
+
+
+def sensitivity(confusion):
+    '''args: confusion
+    return: sensitivity'''
+    classes = confusion.shape[0]
+    sensitivity = np.zeros(classes)
+    for i in range(classes):
+        sensitivity[i] = confusion[i][i] / np.sum(confusion[i])
+    return sensitivity
