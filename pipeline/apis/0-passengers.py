3e9b6052945a13f7cb1d621fcbc976a94bbaaac0
Martin <gichurindungu@gmail.com>
2024-03-07 15:07:46 +0200

    add documentation

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
+"""
+Defines methods to ping the Star Wars API and return the list of ships
+that can hold a given number of passengers
+"""
+
 import requests
+
 def availableShips(passengerCount):
-    '''returns a list of ships that can
-      hold a given number of passengers'''
+    '''
+    This function returns a list of starships that can hold a given number of passengers.
+
+    Parameters:
+    passengerCount (int): The minimum number of passengers a starship must be able to hold.
+
+    Returns:
+    shipslist (list): A list of starship names that can hold at least 'passengerCount' passengers.
+    '''
+
+    # API endpoint to fetch starship data
     url = "https://swapi-api.alx-tools.com/api/starships/?format=json"
+
+    # List to store all starship data
     ships=[]
+
+    # Loop until all pages of the API response have been processed
     while url:
+        # Get the API response
         response = requests.get(url).json()
+
+        # Add the results from the current page to our list
         ships += response.get('results')
+
+        # Get the URL for the next page
         url = response.get('next')
+
+    # List to store the names of starships that can hold the required number of passengers
     shipslist = []
+
+    # Loop through each starship
     for ship in ships:
+        # Get the passenger capacity, removing any commas
         passengers = ship.get('passengers').replace(",", "")
+
+        # If the passenger capacity is known and is sufficient, add the starship to our list
         if passengers != "n/a" and passengers !='unknown':
             if int(passengers) >= passengerCount:
                 shipslist.append(ship.get('name'))
+
+    # Return the list of suitable starships
     return shipslist
