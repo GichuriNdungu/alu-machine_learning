329095cbea4ca039e43b78c178e3352d35e27ec2
Martin <gichurindungu@gmail.com>
2024-03-07 16:17:02 +0200

    correct pep 8

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -5,19 +5,21 @@ list of names of the home planets of all sentient species
 """
 
 import requests
+
+
 def sentientPlanets():
     '''Args:
     return: list of sentient homelands'''
     url = "https://swapi-api.alx-tools.com/api/species/?format=json"
-    species =[]
+    species = []
     while url:
         results = requests.get(url).json()
         species += results.get('results')
         url = results.get('next')
-    planets =[]
+    planets = []
     for specie in species:
         if specie.get('designation') == 'sentient' or\
-        specie.get('classification') == 'sentient':
+                specie.get('classification') == 'sentient':
             planet_url = specie.get('homeworld')
             if planet_url:
                 planet_data = requests.get(planet_url).json()
