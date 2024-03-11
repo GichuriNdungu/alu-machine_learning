380b42826a1d084a5eb7cf7303e40a5d36b273d5
Martin <gichurindungu@gmail.com>
2024-03-11 18:29:30 +0200

    correct group by

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_shows
     JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
+    ON tv_genre.id = tv_show_genres.genre_id
     GROUP BY genre
     ORDER BY number_of_shows DESC;
     
