d44f3c0f03f2187fcdb3d87547e40b97fbe8dc33
Martin <gichurindungu@gmail.com>
2024-03-11 18:43:53 +0200

    sum shows

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -3,4 +3,5 @@ SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
     LEFT JOIN tv_show_ratings
     ON tv_shows.id = tv_show_ratings.show_id
+    GROUP BY tv_shows.title
     ORDER BY tv_shows.title, tv_show_ratings.rate;
