b3c2497eb69727e1edcbb7ffe7587839d82094a6
Martin <gichurindungu@gmail.com>
2024-03-11 18:59:51 +0200

    combine three tables

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,9 @@
+-- combining data from two different tables
+SELECT tv_genres.name, SUM(tv.show_ratings.rate) AS rating
+FROM tv_genres
+    JOIN tv_show_genres 
+        ON tv_genres.id = tv.show_genres.genre_id
+    JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+    GROUP BY tv.genres.name
+    ORDER BY rating;
