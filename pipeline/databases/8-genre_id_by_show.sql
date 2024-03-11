9f461a8dfa7efdcc91fd883569497f44b2f52c52
Martin <gichurindungu@gmail.com>
2024-03-11 17:53:59 +0200

    correct output

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- join and select
+-- query that shows the following:
+-- title, genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title;
+FROM tv_shows
+    JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    ORDER BY tv_shows.title, tv_show_genres.genre_id;
