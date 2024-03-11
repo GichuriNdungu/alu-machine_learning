d1ba0b37ae85b4d0162e2cb34d0542098de31a16
Martin <gichurindungu@gmail.com>
2024-03-11 17:59:05 +0200

    null genres

---
+++ pipeline/databases/9-no_genre.sql
@@ -0,0 +1,8 @@
+-- query that shows the following:
+-- title, genre_id
+SELECT tv_shows.title
+FROM tv_shows
+    LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+    ORDER BY tv_shows.title, tv_show_genres.genre_id;
