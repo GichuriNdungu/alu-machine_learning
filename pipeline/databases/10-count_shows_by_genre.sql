42bec4d78117505daf871cdc8792c9f68d0b4b9b
Martin <gichurindungu@gmail.com>
2024-03-11 18:20:17 +0200

    count shows

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- count shows linked to genres
-SELECT tv_shows.genre_id, COUNT(tv_shows.title) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
 FROM tv_shows
     JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
