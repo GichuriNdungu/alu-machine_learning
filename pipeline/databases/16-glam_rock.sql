03d6c7947eca7e2a93d16d5562af5b73b8bb3552
Martin <gichurindungu@gmail.com>
2024-03-11 23:32:20 +0200

    final

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- glam rock style
-SELECT band_name, IF(split IS NULL,YEAR(CURDATE()), split)-formed AS life_span
+SELECT band_name, IF(split IS NULL, YEAR(CURDATE()), split) - formed AS lifespan
 FROM metal_bands
-WHERE stlye LIKE '%Glam rock%'
-ORDER BY life_span DESC; 
+WHERE style LIKE "%Glam Rock%"
+ORDER BY lifespan DESC;
