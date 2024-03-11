fc95651485b7cb99e22265526404dea0edb10547
Martin <gichurindungu@gmail.com>
2024-03-11 18:41:27 +0200

    rate shows

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows and their ratings
+SELECT tv_shows.title, tv_show_ratings.rate
+FROM tv_shows
+    LEFT JOIN tv_show_ratings
+    ON tv_shows.id = tv_show_ratings.show_id
+    ORDER BY tv_shows.title, tv_show_ratings.rate;
