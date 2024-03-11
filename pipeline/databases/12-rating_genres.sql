8f72ac785a7df9a982a017e2cd38c110799cee8e
Martin <gichurindungu@gmail.com>
2024-03-11 19:04:17 +0200

    combine three tables

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -5,5 +5,5 @@ FROM tv_genres
         ON tv_genres.id = tv_show_genres.genre_id
     JOIN tv_show_ratings
         ON tv_show_ratings.show_id = tv_show_genres.show_id
-    GROUP BY tv.genres.name
+    GROUP BY tv_genres.name
     ORDER BY rating;
