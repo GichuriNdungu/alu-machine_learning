a59d6889de82fc2d37d0f1acb24f7cf1e1c6793e
Martin <gichurindungu@gmail.com>
2024-03-11 18:31:17 +0200

    correct group by

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
--- count shows linked to genres
+-- query to list al genres from hbtn_0d_tvshows
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_shows
-    JOIN tv_show_genres
-    ON tv_genre.id = tv_show_genres.genre_id
-    GROUP BY genre
-    ORDER BY number_of_shows DESC;
-    
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
