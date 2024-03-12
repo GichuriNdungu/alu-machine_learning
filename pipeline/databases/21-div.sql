a8947b3d09fdf974670065f4d0ffe003e76fdf73
Martin <gichurindungu@gmail.com>
2024-03-12 18:03:02 +0200

    first function

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,12 +1,12 @@
---function declaration
+-- first sql function 
 DELIMITER //
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT 
 BEGIN 
     IF (b <=> 0) THEN
-        RETURN 0;
-    END IF;
-    RETURN a/b;
+    RETURN 0 
+    ENDIF
+    RETURN a/b
 END; 
 //
 DELIMITER ;
