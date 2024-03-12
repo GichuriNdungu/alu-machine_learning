3ab3c05ddf2aed2cb68f7c7b0148f702de01bba0
Martin <gichurindungu@gmail.com>
2024-03-12 17:00:38 +0200

    typo

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -7,7 +7,7 @@ DECLARE project_id INT;
 SELECT id INTO project_id FROM Projects WHERE name = project_name;
 IF project_id IS NULL THEN
 INSERT INTO projects (name) VALUES (project_name);
-SET prject_id = LAST_INSERT_ID()
+SET project_id = LAST_INSERT_ID()
 END IF;
 INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END // 
