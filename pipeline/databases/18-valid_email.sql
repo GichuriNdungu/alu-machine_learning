88b607bd2913132b16e934f543ed1b4419ed8d4c
Martin <gichurindungu@gmail.com>
2024-03-12 16:06:14 +0200

    validate emails

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0 
+    END IF; 
+END; 
+// 
+DELIMITER ;
