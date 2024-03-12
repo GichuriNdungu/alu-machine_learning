8fb7e26dc69351e0b7993f9556290b29347623f5
Martin <gichurindungu@gmail.com>
2024-03-12 17:06:16 +0200

    update id properly

--- pipeline/databases/19-bonus.sql
+++ pipeline/databases/19-bonus.sql
@@ -3,12 +3,12 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN 
-DECLARE project_id INT; 
-SELECT id INTO project_id FROM Projects WHERE name = project_name;
-IF project_id IS NULL THEN
-INSERT INTO projects (name) VALUES (project_name);
-SET project_id = LAST_INSERT_ID()
-END IF;
-INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END // 
+    IF NOT EXISTS (SELECT name FROM projects WHERE name=project_name)THEN
+        INSERT INTO projects(name)
+        values(project_name);
+        END IF;
+        INSERT INTO corrections (user_id, project_id, score)
+        VALUES(user_id, (SELECT id FROM projects WHERE name=project_name), score);
+END;
+//
 DELIMITER ;
