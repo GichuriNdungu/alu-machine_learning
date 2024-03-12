dc0c8913925ecce9fb5c89a81f16e48641ee029f
Martin <gichurindungu@gmail.com>
2024-03-12 17:42:35 +0200

    declare new column

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,10 @@
+-- another stored procedure
+DELIMITER // 
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    UPDATE users SET avg_score = avg_score WHERE id = user_id;
+END; 
+//
+DELIMITER;
