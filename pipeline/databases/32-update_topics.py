785a558421559947cf5699279fc88e134eae138f
Martin <gichurindungu@gmail.com>
2024-03-13 00:45:56 +0200

    update topics

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """Defines a function that changes all
  topics of a school document based on the name"""
-def  update_topics(mongo_collection, name, topics):
+
+
+def update_topics(mongo_collection, name, topics):
     """params; 
         mongo_collection: collection to update
         name: name of school to update
         topics: topics to update"""
-    mongo_collection.update_many({'name': name}, 
+    mongo_collection.update_many({'name': name},
                                  {'$set': 'topics': topics})
