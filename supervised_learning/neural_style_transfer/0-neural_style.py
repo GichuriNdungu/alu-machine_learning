743268b1103d56da89dab110b37f775088e44730
Martin <gichurindungu@gmail.com>
2024-05-16 12:09:22 +0200

    clip values to 0-1

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -47,7 +47,7 @@ class NST:
                 'image must be a numpy.ndarray with shape (h, w, 3)')
         else:
             # convert pixel values to range (0-1)
-            image = tf.image.convert_image_dtype(image, tf.float32)
+            # image = tf.image.convert_image_dtype(image, tf.float32)
             # get the initial dimensions
             original_height, original_width = tf.shape(
                 image)[0], tf.shape(image)[1]
