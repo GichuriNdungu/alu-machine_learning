4289c66f48ac241dd53fcc34ca2d5acc762d0836
Martin <gichurindungu@gmail.com>
2024-06-10 17:00:49 +0200

    correct pep8

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''implements the decoder for the transformer model'''
-SelfAttention = __import__('1-self_attention').SelfAttention
 import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
 
 class RNNDecoder(tf.keras.layers.Layer):
     '''class that defines the encoder part of a seq2seq model'''
@@ -9,10 +10,13 @@ class RNNDecoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         '''initializer of class RNNDecoder'''
         super(RNNDecoder, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(input_dim = vocab, output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units=units, recurrent_initializer='glorot_uniform', return_sequences=True, return_state=True)
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=vocab, output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(
+            units=units, recurrent_initializer='glorot_uniform', return_sequences=True, return_state=True)
         self.F = tf.keras.layers.Dense(units=vocab)
         self.attention = SelfAttention(units)
+
     def call(self, x, s_prev, hidden_states):
         '''params:
                 x: tensor containing the previous word in target sequence
