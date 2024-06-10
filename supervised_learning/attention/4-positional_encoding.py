4b8c028773039c9e3daf297574957c0c88749a84
Martin <gichurindungu@gmail.com>
2024-06-10 19:54:37 +0200

    change encoding math

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -2,6 +2,13 @@
 
 import numpy as np
 
+def get_angle(pos, i, dm):
+    '''calculates the angles for positional encoding formulas
+    PE(pos, 2i) = sin(pos / 10000^(2i/dm))
+    PE(pos, 2i +1) = cos(pos / 10000^(2i/dm))'''
+
+    angle_rates = 1 /(10000 ** (i / dm))
+    return pos * angle_rates
 
 def positional_encoding(max_seq_len, dm):
     '''calculates the positional encoding for a transformer
@@ -14,7 +21,6 @@ def positional_encoding(max_seq_len, dm):
     PE = np.zeros([max_seq_len, dm])
     for pos in range(max_seq_len):
         for i in range(0, dm, 2):
-            PE[pos, i] = np.sin(pos / (10000 ** (i/dm)))
-            if i+1 < dm:
-                PE[pos, i+1] = np.cos(pos / (10000**((i+1)/dm)))
+            PE[pos, i] = np.sin(get_angle(pos, i, dm))
+            PE[pos, i+1] = np.cos(get_angle(pos, i, dm))
     return PE
