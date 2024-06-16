correct units in encoder

supervised_learning/attention/0-rnn_encoder.py
@@ -11,11 +11,12 @@ class RNNEncoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         '''initializer of class RNNEncoder'''
         super(RNNEncoder, self).__init__()
+        self.units = units
         self.batch = batch
         self.embedding = tf.keras.layers.Embedding(
             input_dim=vocab, output_dim=embedding)
         self.gru = tf.keras.layers.GRU(
-            units=units, recurrent_initializer='glorot_uniform',
+            units=self.units, recurrent_initializer='glorot_uniform',
             return_sequences=True,
             return_state=True)
 

supervised_learning/attention/test.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import tensorflow as tf
-Encoder = __import__('9-transformer_encoder').Encoder
+RNNEncoder = __import__('0-rnn_encoder').RNNEncoder
 
-encoder = Encoder(6, 512, 8, 2048, 10000, 1000)
-print(encoder.dm)
-print(encoder.N)
-print(encoder.embedding)
-print(encoder.positional_encoding)
-print(encoder.blocks)
-print(encoder.dropout)
-x = tf.random.uniform((32, 10))
-output = encoder(x, True, None)
-print(output)
+encoder = RNNEncoder(1024, 128, 256, 32)
+print(encoder.batch)
+print(encoder.units)
+print(type(encoder.embedding))
+print(type(encoder.gru))
+
+initial = encoder.initialize_hidden_state()
+print(initial)
+x = tf.convert_to_tensor(np.random.choice(1024, 320).reshape((32, 10)))
+outputs, hidden = encoder(x, initial)
+print(outputs)
+print(hidden)