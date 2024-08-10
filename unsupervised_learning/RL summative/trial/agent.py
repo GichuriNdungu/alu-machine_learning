1562f5d81a7b3d765c5de1d620ce056b9ba5dc07
Martin <gichurindungu@gmail.com>
2024-08-10 18:23:39 +0200

    save env

---
+++ unsupervised_learning/RL summative/trial/agent.py
@@ -0,0 +1,17 @@
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+from tensorflow.keras.optimizers import Adam
+from model import model, actions
+from env import env
+import numpy as np
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy, 
+                  nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+    return dqn
+dqn = build_agent(model, actions)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
